1f73f8928e5a7fc51173eb72047c1f2e18493756
JackLeverentz <jackleverentz@gmail.com>
2019-03-25 20:08:34 -0500

    make CREATE, INDEX, and SHOW routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,47 @@
 var express = require("express"),
     router  = express.Router();
+    Blog = require("../models/blog");
 
+
+// Render the list of blogs
 router.get("/blogs", function(req, res){
-    res.render("blogs");
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("blogs", {blogs: blogs});
+        }
+    });
+});
+
+// Render the new blog form
+router.get("/blogs/new", function(req, res){
+    res.render("newBlog");
+});
+
+// CREATE route
+router.post("/blogs", function(req, res){
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW route
+router.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/index");
+        } else {
+            res.render("showBlog", {blog: foundBlog});
+        }
+    });
 });
 
 module.exports = router;
