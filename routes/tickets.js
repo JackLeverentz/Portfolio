9a3e159e795ac70a007ee2b03ebaf15c9a75824e
JackLeverentz <jackleverentz@gmail.com>
2019-03-28 14:17:08 -0500

    add CREATE and SHOW routes

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,13 +2,36 @@ var express   = require("express");
     Ticket   = require("../models/tickets"),
     router   = express.Router();
 
-//INDEX list of tickets
+
+//INDEX find and render the list of tickets
 router.get("/tickets", function(req, res){
     Ticket.find({}, function(err, tickets){
         if(err){
             console.log(err);
         } else {
             res.render("tickets", {tickets: tickets})
+        }
+    });
+});
+
+// SHOW an individual ticket
+router.get("/tickets/:id", function(req, res){
+    Ticket.findById(req.params.id, function(err, foundTicket){
+        if(err){
+            res.redirect("/tickets");
+        } else {
+            res.render("showTicket", {ticket: foundTicket});
+        }
+    });
+});
+
+// DESTROY a individal ticket
+router.delete("/tickets/:id", function(req, res){
+    Ticket.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+           res.redirect("/tickets");
+        } else {
+            res.redirect("/tickets");
         }
     });
 });
