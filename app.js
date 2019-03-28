ec9b646d744b5e10a461512e81f4a41d4b6ca473
JackLeverentz <jackleverentz@gmail.com>
2019-03-28 14:15:17 -0500

    add admin routes and passport config

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     methodOverride = require ("method-override");
     expressSanitizer = require("express-sanitizer"),
+    User   = require("./models/user"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
     Blog = require("./models/blog"),
-    Ticket = require("./models/tickets"),
-    User   = require("./models/user");
-
+    Ticket = require("./models/tickets");
+   
 
 // Route config
 var indexRoutes = require("./routes/index"),
@@ -15,7 +18,7 @@ var indexRoutes = require("./routes/index"),
     blogRoutes = require("./routes/blogs"),
     contactRoutes = require("./routes/contact");
     ticketRoutes  = require("./routes/tickets");
-
+    adminRoutes = require("./routes/admin");
 
 // App config
 app = express();
@@ -28,6 +31,18 @@ app.use(methodOverride("_method"));
 // Local MongoDB conneciton
 mongoose.connect("mongodb://localhost/jackleverentz", { useNewUrlParser: true } );
 
+// Passport Configurations
+app.use(require("express-session")({
+    secret: "jackrobertleverentz",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//passport.use(new LocalStrategy(User.authenticate()));
+//passport.serializeUser(User.serializeUser());
+//passport.deserializeUser(User.deserializeUser());
+
 // Call Routes
 app.use(indexRoutes);
 app.use(aboutRoute);
@@ -35,6 +50,7 @@ app.use(portfolioRoute);
 app.use(blogRoutes);
 app.use(contactRoutes);
 app.use(ticketRoutes);
+app.use(adminRoutes);
 
 // Server listener
 let port = 8080;
