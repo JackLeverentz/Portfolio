83a4a67839f1f197f7b0eb167952326b8f44a095
JackLeverentz <jackleverentz@gmail.com>
2019-03-28 14:15:36 -0500

    add passport to schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
-var mongoose = require("mongoose");
+var mongoose          = require("mongoose"),
+passportLocalMongoose = require("passport-local-mongoose");
 
 // Mongoose model config
 var userSchema = new mongoose.Schema({
     username: String,
     Password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
