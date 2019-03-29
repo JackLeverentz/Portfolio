37ddb5030dc10e32e32cc6e7a5ec17ac7b1451ed
JackLeverentz <jackleverentz@gmail.com>
2019-03-29 11:51:27 -0500

    mover error route to app.js

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ router.get("/", function(req, res){
     res.render("index");
 });
 
-// Render error page for any pages that don't exist
-router.get("*", function(req, res){
-    res.render("error");
-});
+
 
 module.exports = router;
